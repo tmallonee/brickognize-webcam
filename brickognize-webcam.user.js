d0fcd72c9c9e558e03bfe7be1e375b2040f1f29b
Tim <tmallonee@gmail.com>
2023-02-22 19:41:23 -0700

    Update brickognize-webcam.user.js
    purely a test of the updateurl system

--- brickognize-webcam.user.js
+++ brickognize-webcam.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         brickognize webcam
-// @version      1.0.3
+// @version      1.0.4
 // @description  allows for webcam use on brickognize
 // @author       tim
 // @match        https://*.brickognize.com/
